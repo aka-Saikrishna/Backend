cb70eedb759c7a1009e42b5b67f4f035f8f41c75
aka-Saikrishna <vallalasaikrishna04@gmail.com>
2024-05-01 19:37:46 +0530

    connected-database

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 # Chai aur backend Series
 
 Backend Series Model link - [Model Link](https://app.eraser.io/workspace/YkHFDfNHMKwPYPQzfpar)
+
+# ## Important Notes: 
+Two things we have remember while connecting with database:
+1) Whenever we are trying to connect with database we face issue, so to handle those issues we have to use "try catch" or "Promises"
+
+2) Database is always in another continent. When we want to access database it will take some time, it means we have use "Async - await". 
+These is the better apporoach while connecting with database and get access of data from database.
---
+++ src/index.js
@@ -0,0 +1,59 @@
+
+// require('dotenv').config({path: './env'})
+
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Approach 1
+// function connectDB(){}
+
+// connectDB()
+// TO make code better we can use IIFE
+
+( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error)=>
+    { 
+        console.log("Error:", error);
+        throw err 
+    })
+    app.listen(process.env.PORT, ()=> {
+        console.log(`App is listening on pott ${process.env.PORT}`)
+    })
+    } catch (error) {
+        console.log("Error:", error);
+        throw err
+    }
+}) () 
+
+*/
+/* Approach 2 -> 
+    Lets create a new file, write the whole connect to DB code in that file expot the file and then import it here.
+*/
