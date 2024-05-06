211d34382ea1857a7a46fde603bc1df6688b3d07
aka-Saikrishna <vallalasaikrishna04@gmail.com>
2024-05-06 17:25:35 +0530

    custom api response and errorr handling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection FAILED", err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// Promises code
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {}
+// const asyncHandler = (func) => {() => {}}
+
+
+//Try-catch code
+// const asyncHandler = (fn) => async( req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         rex.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
